d9485777c571cad3dbd93a7bd6267d71a543afcd
Aaron Schuman <aaron@aschuman.net>
2017-06-17 14:35:20 -0700

    bootstrapped the forms

--- projects/lake_bottom/lake_bottom/settings.py
+++ projects/lake_bottom/lake_bottom/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'registration'
+    'registration',
+    'bootstrapform',
 ]
 
 MIDDLEWARE_CLASSES = [
