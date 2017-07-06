f3ebe5b37e9710b54189554741f6cfcc7f2d7896
Aaron Schuman <aaron.schuman@urbanairship.com>
2017-07-06 16:31:32 -0700

    cleared sensitive information

--- projects/lake_bottom/lake_bottom/settings.py
+++ projects/lake_bottom/lake_bottom/settings.py
@@ -15,14 +15,16 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Get environment vars
+secret = os.environ.get('DJANGO_SECRET')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # TODO - Load from gcp metadata server / env var
-if os.environ.get('DJANGO_SECRET_KEY') is not None:
-    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+if secret is not None:
+    SECRET_KEY = secret
 else:
     print('No Secret Key Provided')
 
@@ -85,7 +87,7 @@ WSGI_APPLICATION = 'lake_bottom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
---
+++ projects/lake_bottom/lake_bottom/startup_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# env vars from metadata
+SQL_IP=$(curl "http://metadata/computeMetadata/v1/project/attributes/sql-ip" -H "Metadata-Flavor: Google")
+SQL_PW=$(curl "http://metadata/computeMetadata/v1/project/attributes/sql-password" -H "Metadata-Flavor: Google")
+DJANGO_SECRET=$(curl "http://metadata/computeMetadata/v1/project/attributes/django-secret" -H "Metadata-Flavor: Google")
+
+export SQL_IP
+export SQL_PW
+export DJANGO_SECRET
+
+# update, install things as needed
+sudo apt-get update
+sudo apt-get install -y -qq python-pip git
+
+# pull GH repo
+git init
+git pull git@github.com:aschuman0/lakebottom_radio.git
+cd ~/projects/lake_bottom/
+
+# install requirements
+sudo pip install -r requirements.txt
+
+# db migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# stand up server
+sudo python manage.py runserver 0.0.0.0:80
