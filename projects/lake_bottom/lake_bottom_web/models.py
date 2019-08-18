a9653d4af0c39e1861479b2ac98a2ceebf0372bc
Aaron Schuman <aaron@aschuman.net>
2019-08-18 12:49:22 -0400

    adds songs model and changes show to support

--- projects/lake_bottom/lake_bottom_web/models.py
+++ projects/lake_bottom/lake_bottom_web/models.py
@@ -2,20 +2,31 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=1027)
+    artist = models.CharField(max_length=1027)
+    album = models.CharField(max_length=1027)
+    year = models.CharField(max_length=1027)
+
+
 class Show(models.Model):
     name = models.CharField(max_length=255)
     about = models.TextField()
     playlist_file = models.FileField(upload_to='playlists', null=True)
+    songs = models.ManyToManyField(Song)
     date_created = models.DateTimeField()
     published = models.BooleanField(default=True)
     slug = models.SlugField(unique=True)
+
 
 class Page(models.Model):
     title = models.CharField(max_length=255)
     page_body = models.TextField()
     last_updated = models.DateTimeField()
     page_name = models.SlugField(unique=True)
+
 
 class Live(models.Model):
     name = models.CharField(max_length=255)
