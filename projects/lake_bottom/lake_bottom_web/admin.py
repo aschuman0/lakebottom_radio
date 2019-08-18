cd16b233574bea80c27ef29c9509bae9e0def079
Aaron Schuman <aaron@aschuman.net>
2019-08-18 18:53:58 -0400

    adds song

--- projects/lake_bottom/lake_bottom_web/admin.py
+++ projects/lake_bottom/lake_bottom_web/admin.py
@@ -1,22 +1,32 @@
 from django.contrib import admin
 
-from lake_bottom_web.models import Show, Page, Live
+from lake_bottom_web.models import Show, Page, Live, Song
+
 
 # setup automatic slug creation
 class ShowAdmin(admin.ModelAdmin):
     model = Show
-    list_display = ('name', 'about',)
+    list_display = ('name', 'about', 'songs')
     prepopulated_fields = {'slug': ('name',)}
+
 
 class PageAdmin(admin.ModelAdmin):
     model = Page
     list_display = ('page_name', 'title', 'page_body', 'last_updated')
 
+
 class LiveAdmin(admin.ModelAdmin):
     model = Live
     list_display = ('name','is_live')
+
+
+class SongAdmin(admin.ModelAdmin):
+    model = Song
+    list_display = ('title', 'artist, album','year')
+
 
 # Register your models here.
 admin.site.register(Show)
 admin.site.register(Page)
 admin.site.register(Live)
+admin.site.register(Song)
