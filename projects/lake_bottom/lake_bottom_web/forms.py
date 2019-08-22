aa8efe28c87699dd0c0f167d944f139b512db9a8
Aaron Schuman <aaron@aschuman.net>
2019-08-21 20:19:01 -0400

    song form

--- projects/lake_bottom/lake_bottom_web/forms.py
+++ projects/lake_bottom/lake_bottom_web/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 
-from lake_bottom_web.models import Show, Page, Live
+from lake_bottom_web.models import Show, Page, Live, Song
 
 
 class ShowForm(ModelForm):
@@ -19,3 +19,9 @@ class LiveForm(ModelForm):
     class Meta:
         model = Live
         fields = ('is_live', 'stream_url')
+
+
+class SongForm(ModelForm):
+    class Meta:
+        model = Song
+        fields = ('title', 'artist', 'album', 'year', 'genre', 'notes')
