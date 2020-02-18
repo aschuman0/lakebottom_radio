40fab19507b992f344573e8e18d8c56eed02650f
Aaron Schuman <aaron@aschuman.net>
2020-02-18 13:50:52 -0500

    makes generic add_show function

--- projects/lake_bottom/lake_bottom_web/utils.py
+++ projects/lake_bottom/lake_bottom_web/utils.py
@@ -6,6 +6,59 @@ import spotipy
 from lake_bottom_web.models import Song, ShowSongs
 
 
+def add_show(show, song_dict):
+    order = 0
+
+    try:
+        for song in song_dict:
+            title = song.get('Name')
+            artist = song.get('Artist')
+            album = song.get('Album')
+            year = song.get('Year')
+            genre = song.get('Genre')
+
+            # if the song matches, get Song obj from db
+            if Song.objects.filter(title=title,
+                                   artist=artist,
+                                   album=album,
+                                   year=year).exists():
+                target_song = Song.objects.filter(title=title,
+                                                  artist=artist,
+                                                  album=album,
+                                                  year=year).first()
+            # otherwise, create a new Song obj
+            else:
+                target_song = Song(title=title,
+                                   artist=artist,
+                                   album=album,
+                                   year=year,
+                                   genre=genre,
+                                   notes='',
+                                   slug=str(uuid.uuid4()))
+                target_song.save()
+
+            # create song to show association in intermediate table
+            association = ShowSongs(
+                song=target_song,
+                show=show,
+                order=order
+            )
+            association.save()
+            order += 1
+
+        show.save()
+
+        return True
+    except Exception as e:
+        print('Exception when adding show: {}'.format(e))
+
+        return False
+
+
+def show_from_spotify_uri(show, uri, remove=False):
+    pass
+
+
 def show_from_file(show, file_data, remove=False):
     tmp_list = []
     song_order = 0
@@ -15,43 +68,7 @@ def show_from_file(show, file_data, remove=False):
 
     for chunk in file_data.chunks():
         tmp_list.append(chunk.decode('utf-8'))
-    try:
         encoded_file = '\r'.join(tmp_list).split('\r')
         reader = csv.DictReader(encoded_file, delimiter='\t')
-        for song in reader:
-            # if the song exists, create object from db record
-            if Song.objects.filter(title=song['Name'],
-                                   artist=song['Artist'],
-                                   album=song['Album'],
-                                   year=song['Year']).exists():
-                new_song = Song.objects.filter(title=song['Name'],
-                                               artist=song['Artist'],
-                                               album=song['Album'],
-                                               year=song['Year']).first()
-            # if the song does not exist, create new song and commit
-            else:
-                new_song = Song(title=song['Name'],
-                                artist=song['Artist'],
-                                album=song['Album'],
-                                year=song['Year'],
-                                genre=song['Genre'],
-                                notes='',
-                                slug=str(uuid.uuid4()))
-                new_song.save()
 
-            association = ShowSongs(
-                song=new_song,
-                show=show,
-                order=song_order
-            )
-            association.save()
-            song_order += 1
-
-        show.save()
-
-        return True
-
-    except Exception as e:
-        print('Exception when adding show: {}'.format(e))
-
-        return False
+        return add_show(show=show, song_dict=reader)
