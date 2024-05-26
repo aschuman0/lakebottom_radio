19021b02520877904245611b24242b89de2a0684
Aaron Schuman <aaron@aschuman.net>
2024-05-26 15:32:39 -0700

    testing settings

--- projects/lake_bottom/frontend/src/services/loginSlice.ts
+++ projects/lake_bottom/frontend/src/services/loginSlice.ts
@@ -1,18 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
+import type { PayloadAction } from "@reduxjs/toolkit"
 import {
   isLoggedIn,
   setAuthTokens,
   clearAuthTokens,
   getAccessToken,
   getRefreshToken,
-} from "axios-jwt";
-import { axiosInstance } from "./loginApi";
+} from "axios-jwt"
+import { axiosInstance } from "./loginApi"
 export interface LoginState {
-  isLoggedIn: boolean;
-  userId?: string;
-  access?: string;
-  refresh?: string;
+  isLoggedIn: boolean
+  userId?: string
+  access?: string
+  refresh?: string
 }
 
 const initialState: LoginState = {
@@ -20,21 +20,21 @@ const initialState: LoginState = {
   userId: undefined,
   refresh: undefined,
   access: undefined,
-};
+}
 
 const handleLoginAndToken = async (username: string, password: string) => {
   const response = await axiosInstance.post("/api/token/", {
     username: username,
     password: password,
-  });
+  })
   setAuthTokens({
     accessToken: response.data.access,
     refreshToken: response.data.refresh,
-  });
+  })
   console.log(
     `access: ${response.data.access} \n refresh: ${response.data.refresh}`,
-  );
-};
+  )
+}
 
 export const loginSlice = createSlice({
   name: "login",
@@ -44,16 +44,16 @@ export const loginSlice = createSlice({
       state,
       action: PayloadAction<{ username: string; password: string }>,
     ) => {
-      handleLoginAndToken(action.payload.username, action.payload.password);
-      state.isLoggedIn = true;
-      state.userId = action.payload.username;
+      handleLoginAndToken(action.payload.username, action.payload.password)
+      state.isLoggedIn = true
+      state.userId = action.payload.username
     },
     logOut: (state) => {
-      clearAuthTokens();
-      state = initialState;
+      clearAuthTokens()
+      state = initialState
     },
   },
-});
+})
 
-export const { logIn, logOut } = loginSlice.actions;
-export default loginSlice;
+export const { logIn, logOut } = loginSlice.actions
+export default loginSlice
