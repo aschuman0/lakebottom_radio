e4b50b7f9ce683089b383a7e7da9b386deaa77b6
Aaron Schuman <aaron@aschuman.net>
2024-06-02 13:04:09 -0700

    show index start

--- projects/lake_bottom/frontend/src/components/show/ShowIndex.tsx
+++ projects/lake_bottom/frontend/src/components/show/ShowIndex.tsx
@@ -1,12 +1,24 @@
-import * as React from "react";
-import { Heading, Box } from "@chakra-ui/react";
+import * as React from "react"
+import { Heading, Box, Skeleton } from "@chakra-ui/react"
+import ShowListItem from "./ShowListItem"
+import { Show } from "../../services/lakebottomTypes"
+import { useGetShowListQuery } from "../../services/lakebottomApi"
 
 const ShowIndex: React.FC = () => {
+  const { data, isLoading } = useGetShowListQuery()
   return (
     <Box w="100%">
       <Heading size="lg">A Listing of Shows</Heading>
+      <Box marginInlineStart="1vh" marginBlockStart="2vh">
+        <Skeleton isLoaded={!isLoading}>
+          {data &&
+            data.map((show) => {
+              return <ShowListItem show={show} key={show.slug} />
+            })}
+        </Skeleton>
+      </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default ShowIndex;
+export default ShowIndex
