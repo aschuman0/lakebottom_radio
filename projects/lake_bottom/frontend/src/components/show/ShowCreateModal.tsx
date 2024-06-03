7af39e64aecb4bec2f2ffc95bca104e61bc6e0fa
Aaron Schuman <aaron@aschuman.net>
2024-06-02 17:03:51 -0700

    form added

--- projects/lake_bottom/frontend/src/components/show/ShowCreateModal.tsx
+++ projects/lake_bottom/frontend/src/components/show/ShowCreateModal.tsx
@@ -7,6 +7,8 @@ import {
   Box,
   Text,
   Button,
+  Input,
+  Textarea,
 } from "@chakra-ui/react"
 import { AddIcon } from "@chakra-ui/icons"
 
@@ -17,12 +19,74 @@ interface Props {
 
 const ShowCreateModal: React.FC<Props> = (props) => {
   const [buttonLoading, setButtonLoading] = React.useState(false)
+  const [title, setTitle] = React.useState<string>()
+  const [about, setAbout] = React.useState<string>()
+  const [showDate, setShowDate] = React.useState<string>()
+  const [file, setFile] = React.useState<string>()
+
+  const handleCreate = () => {
+    setButtonLoading(true)
+    const showPayload = {
+      title: title,
+      about: about,
+      showDate: showDate,
+      file: file,
+    }
+    console.log(showPayload)
+    setButtonLoading(false)
+    // props.setIsOpen(false)
+  }
+
   return (
     <ModalContent>
       <ModalHeader>Add New Show</ModalHeader>
       <ModalBody>
         <Box height="70vh">
-          <Text>Modal area</Text>
+          <Box marginBlockStart="10px">
+            <Text fontSize="sm" as="b">
+              Title
+            </Text>
+            <Input
+              aria-label="title text area"
+              placeholder="Show Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Box>
+          <Box marginBlockStart="10px">
+            <Text fontSize="sm" as="b">
+              About
+            </Text>
+            <Textarea
+              height="35px"
+              aria-label="about show text area"
+              placeholder="About this show..."
+              value={about}
+              onChange={(e) => setAbout(e.target.value)}
+            />
+          </Box>
+          <Box marginBlockStart="10px">
+            <Text fontSize="sm" as="b">
+              Show Date
+            </Text>
+            <Input
+              type="date"
+              aria-label="show broadcast date"
+              value={showDate}
+              onChange={(e) => setShowDate(e.target.value)}
+            />
+          </Box>
+          <Box marginBlockStart="10px">
+            <Text fontSize="sm" as="b">
+              Choose iTunes / Apple Music File
+            </Text>
+            <Input
+              type="file"
+              aria-label="upload itunes playlist file"
+              value={file}
+              onChange={(e) => setFile(e.target.value)}
+            />
+          </Box>
         </Box>
       </ModalBody>
       <ModalFooter>
@@ -42,7 +106,7 @@ const ShowCreateModal: React.FC<Props> = (props) => {
           colorScheme="green"
           loadingText="Adding"
           isLoading={buttonLoading}
-          onClick={() => setButtonLoading(true)}
+          onClick={() => handleCreate()}
         >
           Add Show
         </Button>
